49f027d6de20f4241638840dae5ef878e595eff3
xuantruongit32 <xuantruongit32@gmail.com>
2024-01-15 09:47:29 +0700

    add bottom navigation bar

---
+++ moneymanager/lib/services/pages/tabs/trans.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Trans extends StatelessWidget {
+  const Trans({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Trans');
+  }
+}
