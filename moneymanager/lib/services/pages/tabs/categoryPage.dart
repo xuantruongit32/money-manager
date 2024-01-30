4276452d554aa4d65486807a5e710ce8df69c829
xuantruongit32 <xuantruongit32@gmail.com>
2024-01-30 19:51:15 +0700

    add button in categoryPage

--- moneymanager/lib/services/pages/tabs/categoryPage.dart
+++ moneymanager/lib/services/pages/tabs/categoryPage.dart
@@ -1,4 +1,6 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:moneyManager/services/functions/transaction_category_manager.dart';
 import 'package:moneyManager/services/pages/others/categoryItem.dart';
 
@@ -16,12 +18,76 @@ class _CategoryPageState extends State<CategoryPage> {
       appBar: AppBar(
         title: Text('Category'),
       ),
-      body: Column(
-        children: TransactionCategoryManager.incomeCategories
-            .map(
-              (e) => CategoryItem(category: e),
-            )
-            .toList(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Income Category',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black45,
+                ),
+              ),
+            ),
+            Column(
+              children: TransactionCategoryManager.incomeCategories
+                  .map(
+                    (e) => CategoryItem(category: e),
+                  )
+                  .toList(),
+            ),
+            const Gap(10),
+            DottedBorder(
+              child: Container(
+                width: MediaQuery.of(context).size.width - 30,
+                child: Center(
+                  child: Text(
+                    '+',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+              padding: EdgeInsets.all(12),
+            ),
+            const Gap(50),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Expense Category',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black45,
+                ),
+              ),
+            ),
+            Column(
+              children: TransactionCategoryManager.expenseCategories
+                  .map(
+                    (e) => CategoryItem(category: e),
+                  )
+                  .toList(),
+            ),
+            const Gap(10),
+            DottedBorder(
+              child: Container(
+                width: MediaQuery.of(context).size.width - 30,
+                child: Center(
+                  child: Text(
+                    '+',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+              padding: EdgeInsets.all(12),
+            ),
+          ],
+        ),
       ),
     );
   }
