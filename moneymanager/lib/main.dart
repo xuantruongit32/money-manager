b9de5f55f3d634281aaf55472a8279e8c246084e
xuantruongit32 <xuantruongit32@gmail.com>
2024-01-31 15:18:55 +0700

    add firebaseStore

--- moneymanager/lib/main.dart
+++ moneymanager/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:moneyManager/network/fire_store.dart';
+import 'package:moneyManager/services/functions/transaction_manager.dart';
 import 'firebase_options.dart';
 import 'package:moneyManager/auth/authPage.dart';
 import 'package:flutter/services.dart';
@@ -21,7 +23,28 @@ class MoneyManager extends StatefulWidget {
   _MoneyManagerState createState() => _MoneyManagerState();
 }
 
-class _MoneyManagerState extends State<MoneyManager> {
+class _MoneyManagerState extends State<MoneyManager>
+    with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused ||
+        state == AppLifecycleState.inactive) {
+      FireStore().addTransactionToFireStore(TransactionManager.trans);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
---
+++ moneymanager/lib/network/fire_store.dart
@@ -0,0 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FireStore {
+  String? getUserId() {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user = auth.currentUser;
+
+    if (user != null) {
+      return user.uid;
+    } else {
+      return null;
+    }
+  }
+
+  Future<void> addTransactionToFireStore(List trans) async {
+    final userId = getUserId();
+
+    CollectionReference transactionsCollection =
+        FirebaseFirestore.instance.collection('users/$userId/transactions');
+    for (var tran in trans) {
+      await transactionsCollection.doc(tran.id).set({
+        'note': tran.note,
+        'amount': tran.amount,
+        'date': tran.date,
+        'id': tran.id,
+        'category': tran.category,
+        'accountId': tran.acc.id,
+        'type': tran.type.toString(),
+      });
+    }
+  }
+}
